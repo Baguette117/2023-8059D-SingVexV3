67064173b8ea460a67629962a2d5482555a34916
student USER <ian@popoheads.com>
2023-09-02 18:11:04 +0800

    Using motor to measure angle instead of rotation sensor

--- src/mechlib.cpp
+++ src/mechlib.cpp
@@ -1,24 +1,39 @@
 #include "main.h"
-#define kp .180
+#define kp 4
+#define kd 8
+//Note that values are set for degrees not centidegrees, must change when swapping to rotation sensor
 
 void cataPID(void *ignore){
     Motor cata (cataPort, MOTOR_GEAR_GREEN, false, MOTOR_ENCODER_DEGREES);
     Rotation rotation (rotationPort);
     Controller master (CONTROLLER_MASTER);
 
-    double err;
+    double err, targ = 0;
     while (true) {
-        if (master.get_digital_new_press(DIGITAL_R2)){
-            cata.move(-127);
-            delay(750);
-        } else if (master.get_digital(DIGITAL_R1)){
-            cata.move(-50);
-        } else {
-            err = 0 - rotation.get_position();
+        // if (master.get_digital_new_press(DIGITAL_R2)){
+        //     cata.move(-127);
+        //     delay(750);
+        // } else if (master.get_digital(DIGITAL_R1)){
+        //     cata.move(-50);
+        // } else {
+        //     err = 0 - rotation.get_position();
 
+        //     cata.move(err*kp);
+        // }
+
+        if (master.get_digital_new_press(DIGITAL_R2)){
+            targ += 1080;
+        }
+        
+        if (master.get_digital(DIGITAL_R1)){
+            cata.move(70);
+            targ = cata.get_position();
+        } else {
+            err = targ - cata.get_position();
             cata.move(err*kp);
         }
 
+        
         delay(15);
     }
 }
